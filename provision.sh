2537956c360c7e17427a8b553d4a4ca0b29c213a
Maros Kukan <maros.kukan@me.com>
2021-01-25 21:33:44 +0100

    Created a new default nginx www root directory in /vagrant shared folder and repointed /var/www/html

--- provision.sh
+++ provision.sh
@@ -4,5 +4,10 @@ apt-get -y update
 # Install nginx
 apt-get -y install nginx
 
+# Repoint www root folder
+rm -rf /var/www/html
+ln -s /vagrant/www/html /var/www/html
+
 # Start nginx service
 service nginx start
+
